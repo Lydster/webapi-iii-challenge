87ee0a698d1ce08f3f593bef94340f2a1e14cde3
lydiathornton <lydia.d.thornton@gmail.com>
2019-03-28 13:11:47 -0700

    added starter

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // code away!
+require("dotenv").config();
+
 const server = require("./server.js");
 
-server.listen(8000, () => {
-  console.log("Server is running on port 8000!");
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}!`);
 });
